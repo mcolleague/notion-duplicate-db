1c60a5a46f8d2c62f71ab167659c8f3e2805c2ac
Max Woodman <maxwoodman@hotmail.co.uk>
2022-01-27 23:23:24 +0000

    misc

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
-import { Client } from "@notionhq/client"
-
+const { Client } = require('@notionhq/client')
 const notion = new Client({ auth: process.env.NOTION_KEY })
+const AWS = require('aws-sdk');
 
-async function migrateSharedTasks(){
-    const dbByTitleFilter = (str) => ({ title: [{plain_text}] }) => plain_text === str 
-
-    const dbsSearchObj = { 
-        query: '', 
-        filter: {
-            property: 'object',
-            value: 'database'
-        }
+const dbByTitleFilter = (str) => ({ title: [{plain_text}] }) => plain_text === str
+const getTasksQueryObj = (dbId) => ({
+    database_id: dbId,
+    filter: {
+        or: [
+            {
+                property: "Is shared",
+                formula: {
+                    checkbox: {
+                        equals: true
+                    }
+                }
+            }
+        ]
     }
+})
+const dbsSearchObj = { 
+    query: '', 
+    filter: {
+        property: 'object',
+        value: 'database'
+    }
+}
+const cloneTaskToSharedDecorator = (database_id) => (task, i) => {
+    const props = [
+        "Status",
+        "Due",
+        "Tags",
+        "Project",
+        "Theme",
+        "Assignee",
+    ].reduce((prev, key) => {
+        const matchedProp = task.properties[key]
+        const type = matchedProp?.type
+        return matchedProp 
+            ? { ...prev, [key]: {[type]: matchedProp[type]} }
+            : prev
+    }, {
+        title: { 
+            title: [
+                {
+                    "text": {
+                        "content": task.properties.Name.title[0].plain_text
+                    }
+                }
+            ]
+        }           
+    })
+
+    notion.pages.create({
+        parent: { database_id },
+        properties: props      
+    })     
+}
+
+exports.handler = async (event, context, callback) => {
     const dbsSearch = await notion.search(dbsSearchObj)
     const dbs = dbsSearch.results
     const dbsMap = {
@@ -27,112 +73,10 @@ async function migrateSharedTasks(){
         }
     }
 
-    const tasksQuery = await notion.databases.query(
-        {
-            database_id: dbsMap.tasks.main,
-            filter: {
-                or: [
-                    {
-                        property: "Is shared",
-                        formula: {
-                            checkbox: {
-                                equals: true
-                            }
-                        }
-                    }
-                ]
-            }
-        }
-    )
+    const tasksQueryObj = getTasksQueryObj(dbsMap.tasks.main)
+    const tasksQuery = await notion.databases.query(tasksQueryObj)
     const tasks = tasksQuery.results
 
-    console.log(tasks[0].properties)
-
-    tasks.forEach((task, i) => {
-        const props = [
-            "Status",
-            "Due",
-            "Tags",
-            "Project",
-            "Theme",
-            "Assignee",
-        ].reduce((prev, key) => {
-            const matchedProp = task.properties[key]
-            const type = matchedProp?.type
-            return matchedProp 
-                ? { ...prev, [key]: {[type]: matchedProp[type]} }
-                : prev
-        }, {
-            title: { 
-                title: [
-                    {
-                        "text": {
-                            "content": task.properties.Name.title[0].plain_text
-                        }
-                    }
-                ]
-            }           
-        })
-
-        notion.pages.create({
-            parent: { database_id: dbsMap.tasks.shared },
-            properties: props      
-        })        
-    })
+    tasks.forEach(cloneTaskToSharedDecorator(dbsMap.tasks.shared))
+    return 'Success'
 }
-
-async function listDatabases() {
-    search('', { value: 'database', property: 'object' })
-        .then((dbs) => {
-            const info = dbs.map(({title, id}) => ({
-                id,
-                title: title[0].plain_text
-            }))
-            console.log(info)
-        })
-}
-
-async function search(query, filter, sort) {
-    let search = { query }
-    if (filter) search = { ...search, filter }
-    if (sort) search = { ...search, sort }
-
-    const response = await notion.search(search)
-    const results = await response.results
-    return results
-}
-
-async function getItems(database_id, filter, sorts) {
-    const response = await notion.databases.query({
-        database_id
-    });
-    console.log(response)
-}
-
-async function addItem(databaseId, title) {
-  try {
-    const response = await notion.pages.create({
-      parent: { database_id: databaseId },
-      properties: {
-        title: { 
-          title:[
-            {
-              "text": {
-                "content": title
-              }
-            }
-          ]
-        }
-      },
-    })
-    console.log(response)
-    console.log("Success! Entry added.")
-  } catch (error) {
-    console.error(error.body)
-  }
-}
-
-// addItem(XXX, "Yurts in Big Sur, California")
-// getItems(databaseId)
-// listDatabases()
-migrateSharedTasks()
